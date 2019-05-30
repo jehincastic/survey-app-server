1f783daec2601def559d199e62b7d17900d673ad
Castic Jehin R <cast2138@global.chainsys.com>
2019-05-30 13:40:03 +0530

    'final'

--- routes/survey.js
+++ routes/survey.js
@@ -449,7 +449,6 @@ router.get("/send", (req, res) => {
                     });
                     const uniqueResponded = responded[0];
                     let done = true;
-                    console.log(emails);
                     emails.forEach((rec, i) => {
                         if (!Number(uniqueResponded[i])) {
                             const mailOptions = {
@@ -467,6 +466,14 @@ router.get("/send", (req, res) => {
                                     res.json({ message: err });
                                 }
                             });
+                            if (i === emails.length - 1) {
+                                console.log(done);
+                                if (done) {
+                                    res.json({ message: "Send Successfully" });
+                                } else {
+                                    res.json({ message: "failed to send" });
+                                }
+                            }
                         }
                     });
                 }
